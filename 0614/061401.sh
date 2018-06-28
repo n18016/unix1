cf22770945439c991eb81e59f4e8b1f2c7a98e68
masato <m18016@std.it-college.ac.jp>
2018-06-28 04:17:24 +0000

    06/14修正

--- 0614/061401.sh
+++ 0614/061401.sh
@@ -6,8 +6,7 @@ b=$((year % 100))
 c=$((year % 400))
 
 if [ $a -eq 0 ] && [ ! $b -eq 0 ] || [ $c -eq 0 ]; then
-	    echo "閏年"
-		else
-					echo "閏年ではない"
-				fi
+	 echo "閏年"
+else
+					      echo "閏年ではない"
 fi
