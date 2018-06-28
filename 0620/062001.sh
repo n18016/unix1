430084f0653c849c8cfd809ecf26f0e3a43e6fae
masato <m18016@std.it-college.ac.jp>
2018-06-28 04:45:08 +0000

    06/20修正

--- 0620/062001.sh
+++ 0620/062001.sh
@@ -2,17 +2,17 @@
 
 for i in $(seq 1 100)
 do
-		    if [ $((i % 15)) -eq 0 ]; then
-				echo "FIZZBUZZ" | tr '\n' ','
+	    if [ $((i % 15)) -eq 0 ]; then
+			echo "FIZZBUZZ" | tr '\n' ','
 
 elif [ $((i % 5)) -eq 0 ]; then
-				echo "BUZZ" | tr '\n' ','
+	    echo "BUZZ" | tr '\n' ','
 
-elif [ $((i % 3)) -eq 0 ]; then
-				echo "FIZZ" | tr '\n' ','
+elif [ $((i % 3)) -eq 0 ];then
+	    echo"FIZZ" | tr '\n' ','
 
 else
-				echo $i | tr '\n' ','
+	    echo $i | tr '\n' ','
 
 fi
 
