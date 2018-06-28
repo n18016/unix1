acd3467b775e13b9644c6675f4081297617cf4cf
masato <m18016@std.it-college.ac.jp>
2018-06-28 04:10:18 +0000

    06/13修正

--- 0613/061301.sh
+++ 0613/061301.sh
@@ -1,1 +1,7 @@
-echo 20180613 20180910 | tr ' ' \n' | date-f - +%s | tr '\n' ' ' | awk '{print $2-$1}' |awk '{print $1/(60*60*24)}'
+#!/bin/bash
+
+today=$(date '+%s')
+summer_vacation=$(date -d '20180910' '+%s')
+ret=$(($summer_vacation - $today))
+ret=$((ret/87400+1))
+echo $ret
