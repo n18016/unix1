8fde25c0b9bf5103f7d3a30d6a203e4fb6f211ed
masato <m18016@std.it-college.ac.jp>
2018-06-28 04:23:43 +0000

    06/20修正

--- 0620/062001.sh
+++ 0620/062001.sh
@@ -1,16 +1,19 @@
+#!/bin/bash
+
 for i in $(seq 1 100)
 do
-	r3='expr $i % 3'
-	r5='expr $i % 5'
-	r15='expr $i % 15'
-  if [ $r15 -eq 0 ]; then
-		echo "FIZZBUZZ"
-	elif [ $r3 -eq 0 ]; then
-		echo "FIZZ"
-	elif [ $r5 -eq 0 ]; then
-		echo "BUZZ"
-	else
-		echo $i
-	fi
-done
+		    if [ $((i % 15)) -eq 0 ]; then
+				echo "FIZZBUZZ" | tr '\n' ','
 
+elif [ $((i % 5)) -eq 0 ]; then
+				echo "BUZZ" | tr '\n' ','
+
+elif [ $((i % 3)) -eq 0 ]; then
+				echo "FIZZ" | tr '\n' ','
+
+else
+				echo $i | tr '\n' ','
+
+fi
+
+done
